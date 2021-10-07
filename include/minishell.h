c8bd9d4413eb7c657b666b5e75c299e8036a6350
llalba <llalba@student.42.fr>
2021-10-07 14:58:18 +0200

    norm fix

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmonbeig <fmonbeig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llalba <llalba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 13:52:32 by fmonbeig          #+#    #+#             */
-/*   Updated: 2021/10/07 12:24:45 by fmonbeig         ###   ########.fr       */
+/*   Updated: 2021/10/07 14:51:43 by llalba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,16 @@
 // Idee de struct principale
 typedef struct s_data
 {
-    int     len_line;
-    int     in; // no < = 0 , < = 1 , << = 2
-    int     out; // no > = 0 , > = 1 , >> = 2
-    char    *infile_name; //nom du infile . e.g  test < cat -e    --> infile = test
-    char    *outfile_name;//       outfile        cat -e > test   --> outfile = test
-	char    *infile; // permet de recup le fd de l ouverture du infile
-    char    *outfile; // 										outfile
-    t_pipe  *pipe;
+	int		len_line;
+	int		in; // no < = 0 , < = 1 , << = 2
+	int		out; // no > = 0 , > = 1 , >> = 2
+	char	*infile_name; //nom du infile . e.g  test < cat -e    --> infile = test
+	char	*outfile_name;//       outfile        cat -e > test   --> outfile = test
+	char	*infile; // permet de recup le fd de l ouverture du infile
+	char	*outfile; // 										outfile
+	t_pipe	*pipe;
 	t_cmd	*l_cmd;
-}       t_data;
+}		t_data;
 
 // struct pour le pipe
 
@@ -88,4 +88,3 @@ typedef struct s_pipe
 
 
 #endif
-
