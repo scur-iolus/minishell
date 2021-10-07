75698b7b81b96ac60fe192e1948350c06ced98d7
llalba <llalba@student.42.fr>
2021-10-07 14:58:09 +0200

    added a src folder

---
+++ src/minishell.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llalba <llalba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 13:59:45 by fmonbeig          #+#    #+#             */
+/*   Updated: 2021/10/07 14:55:47 by llalba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/minishell.h"
+
+int main(int argc, char **argv, char **env)
+{
+	char *line;
+	(void)argv;
+	(void)env;
+	if (argc == 1)
+	{
+		while (1)
+		{
+			line = readline("Mishell>");
+			if  (ft_strlen(line) > 0)
+				add_history(line);  // free quelque part ??
+
+			// fonction de parsing
+
+
+			// fonction qui fait la ou les commandes
+			printf("line read --> %s\n", line);
+
+			//multipipe(structure principale);
+			//fonction qui free tout avant la prochaine ligne de commande
+			free(line);
+			line = NULL;
+		}
+		return (0);
+	}
+	printf("ERROR : no argument is required");
+	return (1);
+}
