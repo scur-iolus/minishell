e848587b809f651036aa09f30c5dde8a83ef040d
lalba <lalba@student.42.fr>
2021-11-09 13:54:06 +0100

    in progress

--- src/s_cmd_list.c
+++ src/s_cmd_list.c
@@ -6,7 +6,7 @@
 /*   By: llalba <llalba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 17:14:06 by llalba            #+#    #+#             */
-/*   Updated: 2021/10/22 17:42:27 by llalba           ###   ########.fr       */
+/*   Updated: 2021/11/08 16:11:05 by llalba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	build_cmd_list(t_data *data, char *line)
 {
-	char 	**ptr;
-	char	*tmp;
+	char	**ptr;
+	t_cmd	*head;
 	size_t	i;
 
 	i = 0;
+	head = 0;
 	ptr = ft_split(line, '|');
 	while (ptr[i])
 	{
-		tmp = ptr[i];
-		ptr[i] = ft_strtrim(ptr[i], ' 	');
-		free(tmp);
+		ft_lstadd_back_cmd(&head, ft_lstnew_cmd(ft_strtrim(ptr[i], ' 	')));
+		free(ptr[i]);
 		i++;
 	}
 
