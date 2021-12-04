ae2a6518f2e54b7aff33b975e668b720fd9bc45d
Florian MONBEIG <fmonbeig@e2r3p19.clusters.42paris.fr>
2021-12-04 14:49:37 +0100

    ptit changement par rapport a l ouverture des infiles

--- src/multipipe/process.c
+++ src/multipipe/process.c
@@ -6,7 +6,7 @@
 /*   By: fmonbeig <fmonbeig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 19:20:04 by fmonbeig          #+#    #+#             */
-/*   Updated: 2021/12/03 18:34:00 by fmonbeig         ###   ########.fr       */
+/*   Updated: 2021/12/04 14:41:58 by fmonbeig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	first_process(t_data *data, t_pipe *pipe, t_cmd *cmd)
 		exit(data->exit_status);
 	}
 	// if (access(cmd->cmd_path, X_OK))
-	// 	err_free_command_not_found(FILE_NOT_FOUND, data, 0); //FIXME changer ca car ca ne met pas le bon message d erreur Command noot found
+	// 	err_free_command_not_found(FILE_NOT_FOUND, data, 0);
 	if ((cmd->cmd_path == NULL ||
 				execve (cmd->cmd_path, cmd->cmd, data->env) == -1))
 		command_failed(data, pipe, cmd);
@@ -43,9 +43,9 @@ void	first_process(t_data *data, t_pipe *pipe, t_cmd *cmd)
 void	middle_process(t_data *data, t_pipe *pipe, t_cmd *cmd)
 {
 	close_fd_middle_process(pipe);
-	open_infile_and_heredoc(cmd);
 	dup2(pipe->end[pipe->i][0], STDIN_FILENO);
 	dup2(pipe->end[pipe->i + 1][1], STDOUT_FILENO);
+	open_infile_and_heredoc(cmd);
 	close(pipe->end[pipe->i + 1][1]);
 	close(pipe->end[pipe->i][0]);
 	if (cmd->ok == 0)
@@ -66,10 +66,10 @@ void	middle_process(t_data *data, t_pipe *pipe, t_cmd *cmd)
 void	last_process(t_data *data, t_pipe *pipe, t_cmd *cmd)
 {
 	close_fd_last_process(pipe);
-	open_infile_and_heredoc(cmd);
 	if (cmd->outfile)
 		dup_outfile(cmd, pipe);
 	dup2(pipe->end[pipe->i][0], STDIN_FILENO);
+	open_infile_and_heredoc(cmd);
 	close(pipe->end[pipe->i][0]);
 	if (cmd->ok == 0)
 		command_failed_because_of_file_opening(data);
